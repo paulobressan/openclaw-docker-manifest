a8d9b6c6133fcdffbc4c98bbf84bed652196aed9
paulobressan <paulo.bressan@outlook.com>
2026-02-05 22:17:32 -0300

    fix: fixed nginx entrypoint

--- nginx/entrypoint.sh
+++ nginx/entrypoint.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 set -e
-printf '%s:%s\n' "$BASIC_AUTH_USER" "$(htpasswd -nbB "" "$BASIC_AUTH_PASSWORD" | cut -d: -f2)" > /etc/nginx/.htpasswd
+printf '%s:%s\n' "$BASIC_AUTH_USER" "$(mkpasswd -m sha-512 "$BASIC_AUTH_PASSWORD")" > /etc/nginx/.htpasswd
 exec /docker-entrypoint.sh nginx -g 'daemon off;'
